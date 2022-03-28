6197074c951105aa25f8a87207214524bf1ce7d1
Azabell1993 <Azabell1993>
2022-03-28 21:14:25 +0900

    cmt

--- example02_calculator_structpointer/CalculatorStruct.c
+++ example02_calculator_structpointer/CalculatorStruct.c
@@ -11,7 +11,7 @@ struct _CALC_DATA {
     double result;
 
     double    (*SETCUL)(struct _CALC_DATA *this, double operand1, double operand2, char operator, double result);
-    double     (*GETCUL)(const struct _CALC_DATA *this);
+    double    (*GETCUL)(const struct _CALC_DATA *this);
 };
 
 double
@@ -124,3 +124,5 @@ int main()
     //소멸자
     Destory(&test4);
 }
+
+
---
+++ example02_calculator_structpointer/CalculatorStruct_run.bat
@@ -0,0 +1,1 @@
+gcc CalculatorStruct.c -o CalculatorStruct
