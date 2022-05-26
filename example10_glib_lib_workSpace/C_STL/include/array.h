16022c13b7d93626383f60baf9c938b3f179b6ba
Azabell1993 <moreirly@naver.com>
2022-05-26 14:30:35 +0900

    cmt

---
+++ example10_glib_lib_workSpace/C_STL/include/array.h
@@ -0,0 +1,15 @@
+//
+// Created by azabell on 2022/05/26.
+//
+
+#ifndef C_STL_ARRAY_H
+#define C_STL_ARRAY_H
+
+void test_func(void);
+
+void test_func(void)
+{
+    puts("test func");
+}
+
+#endif //C_STL_ARRAY_H
---
+++ example10_glib_lib_workSpace/C_STL/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "include/array.h"
+int main() {
+    puts("STL_TEST");
+    //test_func();
+
+    return 0;
+}
