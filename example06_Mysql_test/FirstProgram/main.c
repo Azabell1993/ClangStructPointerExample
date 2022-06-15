ef7d12b0bfb30c470cf4b137dfc0d167c897b46a
Azabell1993 <moreirly@naver.com>
2022-06-16 00:15:45 +0900

    cmt

---
+++ example06_Mysql_test/FirstProgram/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <mysql.h>
+#include <glib.h>
+#include <gtk/gtk.h>
+#include "include/student.h"
+
+say_hello(hellofunc, int)
+
+int main(int argc, char* argv[])
+{
+    hellofunc hello = new_hellofunc();
+    hello.sethello(&hello, 30);
+    hello.gethello(&hello);
+}
